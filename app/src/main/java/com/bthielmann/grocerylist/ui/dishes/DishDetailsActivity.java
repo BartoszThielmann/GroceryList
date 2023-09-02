17a7e0b6697bda58a864e48847d64cce85cfbf1f
Bartosz Thielmann <bartoszthielmann@gmail.com>
2023-09-02 15:46:55 +0200

    Add confirmation dialog when removing dish

--- app/src/main/java/com/bthielmann/grocerylist/ui/dishes/DishDetailsActivity.java
+++ app/src/main/java/com/bthielmann/grocerylist/ui/dishes/DishDetailsActivity.java
@@ -1,5 +1,7 @@
 package com.bthielmann.grocerylist.ui.dishes;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,22 +9,38 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.bthielmann.grocerylist.R;
 
 public class DishDetailsActivity extends AppCompatActivity {
 
+    public static class RemoveDialogFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.question_remove_dish)
+                    .setPositiveButton(R.string.confirm,
+                            (dialogInterface, i) -> ((DishDetailsActivity)getActivity()).doPositiveClick())
+                    .setNegativeButton(R.string.cancel,
+                            (dialogInterface, i) -> ((DishDetailsActivity)getActivity()).doNegativeClick());
+            return builder.create();
+        }
+    }
+
     private DishDetailsViewModel mDishDetailsViewModel;
+    private String mDishName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dish_details);
-        TextView dishName = findViewById(R.id.dish_name);
+        TextView dishNameView = findViewById(R.id.dish_name);
 
         Intent intent = getIntent();
-        String dish_name = intent.getStringExtra("dish_name");
-        dishName.setText(dish_name);
+        mDishName = intent.getStringExtra("dish_name");
+        dishNameView.setText(mDishName);
 
         // TODO: Get ingredients from Repository
 
@@ -31,12 +49,24 @@ public class DishDetailsActivity extends AppCompatActivity {
 
         final Button removeButton = findViewById(R.id.button_remove);
         removeButton.setOnClickListener(view -> {
-            mDishDetailsViewModel.deleteDish(dish_name);
-            Toast.makeText(
-                    getApplicationContext(),
-                    "Dish removed!",
-                    Toast.LENGTH_LONG).show();
-            finish();
+            showRemoveDialog();
         });
+    }
+
+    private void showRemoveDialog() {
+        DialogFragment removeDialog = new RemoveDialogFragment();
+        removeDialog.show(getSupportFragmentManager(), "remove_dialog");
+    }
+
+    private void doPositiveClick() {
+        mDishDetailsViewModel.deleteDish(mDishName);
+        finish();
+    }
+
+    private void doNegativeClick() {
+        Toast.makeText(
+                getApplicationContext(),
+                "Cancelled!",
+                Toast.LENGTH_LONG).show();
     }
 }
